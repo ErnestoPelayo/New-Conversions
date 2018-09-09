38c9fcf8a843847ec4cba0a96f1ddc686c017bd7
[Ernesto] <[ernesto.p@itmexicali.edu.mx]>
2018-09-08 23:24:39 -0700

    the methods of view added

---
+++ View.java
@@ -0,0 +1,41 @@
+package packageConversions;
+
+public class View {
+
+
+    public void Menu(){
+
+        System.out.println("-----Menu-----");
+        System.out.println("1.-Celsius to Fahrenheit");
+        System.out.println("2.-Celsius to Kelvin");
+        System.out.println("3.-Fahrenheit to Celsius");
+        System.out.println("4.-Fahrenheit to Kelvin");
+        System.out.println("5.-Kelvin to Celsius");
+        System.out.println("6.-Kelvin to Fahrenheit");
+        System.out.println("7.-Exit");
+        System.out.println("Option: ");
+
+    }
+    public void CtoF(){
+        System.out.println("Celsius To Fahrenheit");
+    }
+    public void CtoK(){
+        System.out.println("Celsius To Kelvin");
+
+    }
+    public void FtoC(){
+        System.out.println("Fahrenheit To Celsius");
+    }
+    public  void FtoK(){
+        System.out.println("Fahrenheit To Kelvin");
+    }
+    public void KtoC(){
+        System.out.println("Kelvin To Celsius");
+    }
+    public void KtoF(){
+        System.out.println("Kelvin To Fahrenheit");
+    }
+
+
+
+}
